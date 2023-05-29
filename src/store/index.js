7726bdabfcbfd1643cf2d229f09a12abf8ecc832
NikaKhiz <nika.khizambareli@redberry.ge>
2023-05-29 17:24:10 +0400

    refactor: store modifie store structure according to endpoint scheme

--- src/store/index.js
+++ src/store/index.js
@@ -7,23 +7,27 @@ const store = createStore({
   state() {
     return {
       questionaire: JSON.parse(sessionStorage.getItem("questionaire")) ?? {
-        identification: {
-          first_name: "",
-          last_name: "",
-          email: "",
+        first_name: "",
+        last_name: "",
+        email: "",
+        had_covid: "",
+        had_antibody_test: null,
+        covid_sickness_date: "",
+        antibodies: {
+          test_date: "",
+          number: 0,
         },
-        covid: {
-          had_covid: "",
-          had_antibody_test: null,
-          covid_sickness_date: "",
-          antibodies: {
-            test_date: "",
-            number: null,
-          },
-        },
+        had_vaccine: null,
+        vaccination_stage: "",
+        i_am_waiting: "",
+        non_formal_meetings: "",
+        number_of_days_from_office: null,
+        what_about_meetings_in_live: "",
+        tell_us_your_opinion_about_us: "",
       },
-      page: JSON.parse(sessionStorage.getItem("page")) ?? 1,
+      page: JSON.parse(sessionStorage.getItem("page")) ?? 0,
       showCovidAdditionalQuestions: false,
+      showAdditionalVaccinationQuestions: false,
     };
   },
   getters: getters,
