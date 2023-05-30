a5978029f480591ee8e3cfe7701602113d9c34c4
NikaKhiz <nika.khizambareli@redberry.ge>
2023-05-30 15:03:09 +0400

    style: suggestions and greatings style suggestions and greatings pages add necessary routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,8 @@ import WelcomeView from "@/views/welcome/WelcomeView.vue";
 import Identificationview from "@/views/identification/IdentificationView.vue";
 import CovidView from "@/views/covid/CovidView.vue";
 import VaccinationView from "@/views/vaccination/VaccinationView.vue";
-
+import SuggestionsView from "@/views/suggestions/SuggestionsView.vue";
+import GreatingsView from "@/views/greatings/GreatingsView.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -34,7 +35,12 @@ const router = createRouter({
     {
       name: "suggestions",
       path: "/suggestions",
-      component: null,
+      component: SuggestionsView,
+    },
+    {
+      name: "greatings",
+      path: "/greatings",
+      component: GreatingsView,
     },
   ],
 });
---
+++ src/views/greatings/GreatingsView.vue
@@ -0,0 +1,47 @@
+<script setup>
+import IconStarGreatings from "@/components/icons/IconStarGreatings.vue";
+import { ref, onMounted } from "vue";
+
+const StarsAnimation = ref(false);
+
+onMounted(() => {
+  setTimeout(() => {
+    StarsAnimation.value = true;
+  }, 1500);
+});
+</script>
+<template>
+  <div
+    class="min-w-screen min-h-screen bg-neutralBlack text-white flex items-center justify-center font-bpg"
+  >
+    <transition
+      appear
+      enter-from-class="opacity-0"
+      enter-active-class="transition-[opacity] duration-[1000ms] delay-[500ms]"
+    >
+      <div class="relative text-6xl font-bold tracking-wide">
+        <h2>მადლობა</h2>
+        <transition
+          appear
+          enter-from-class="translate-y-[80px] translate-x-[10px]"
+          enter-active-class="transition-[all] duration-[200ms]"
+        >
+          <icon-star-greatings
+            v-if="StarsAnimation"
+            class="absolute -top-20 left-5 w-12"
+          />
+        </transition>
+        <transition
+          appear
+          enter-from-class="-translate-y-[50px] -translate-x-[40px]"
+          enter-active-class="transition-[all] duration-[200ms]"
+        >
+          <icon-star-greatings
+            v-if="StarsAnimation"
+            class="absolute -bottom-12 right-2 w-8"
+          />
+        </transition>
+      </div>
+    </transition>
+  </div>
+</template>
