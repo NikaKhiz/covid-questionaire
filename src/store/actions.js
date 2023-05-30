f734076c5404a3ac79303d257c9535c413f742f7
NikaKhiz <nika.khizambareli@redberry.ge>
2023-05-30 15:14:15 +0400

    refactor: modifie store create necessary mutation and action for sending data

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,8 +1,18 @@
+import axios from "@/plugins/axios";
 export default {
   setQuestionaire(context, payload) {
     context.commit("setQuestionaire", payload);
   },
   setPage(context, payload) {
     context.commit("setPage", payload);
+  },
+  async modifieDataToSend(context, payload) {
+    context.commit("modifieDataToSend", payload);
+    try {
+      const response = await axios.post("/create", payload);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
